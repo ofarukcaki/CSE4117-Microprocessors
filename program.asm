19aec106cc2a2d619879d2b525d4845673b07dd8
Ömer Çakı <omerfarukcaki@gmail.com>
2020-12-14 23:39:49 +0300

    Update program.asm

--- program.asm
+++ program.asm
@@ -1,31 +1,21 @@
 .data
-    terms:40
-    resul:40
+            terms: 40
+            result: 40
 .code
-    ldi 4
-    ldi 0 terms
-
-mainLoop:
-    ld 0 0
-    mov 1 0
-    mov 2 0
-    ldi 3 0
-
-loop:
-    add 3 3 1
-    dec 2
-    jz out
-    jmp loop
-
-out:
-    add 4 4 3
-    dec 0
-    jz out2
-    jmp mainLoop
-
-out2:
-    ldi 5 result
-    st 5 4
-
-end:
-    jmp end
+            ldi 4 0
+            ldi 0 terms
+mainLoop    ld 0 0
+            mov 1 0
+            mov 2 0
+            ldi 3 0
+loop        add 3 3 1
+            dec 2
+            jz out
+            jmp loop
+out         add 4 4 3
+            dec 0
+            jz out2
+            jmp mainLoop
+out2        ldi 5 result
+            st 5 4
+end         jmp end
