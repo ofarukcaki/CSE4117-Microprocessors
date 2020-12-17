9e030cb2d59bfc927f77db60ce0a48450ea36705
Ömer Çakı <omerfarukcaki@gmail.com>
2020-12-17 19:46:21 +0300

    update

--- match_stick_top.v
+++ match_stick_top.v
@@ -9,6 +9,8 @@ module match_stick_top (
 reg [11:0] total;
 reg [11:0] minus;
 reg [1:0] state;
+assign buttons = {~pushbuttons[0],~pushbuttons[1]};
+
 
 wire [1:0] pbs;
 assign pbs=pushbuttons;
@@ -19,6 +21,9 @@ assign ds=dipswitchess;
 reg [3:0] playerNum= 4'b0001;
 reg [15:0] dataModule;
 
+reg [11:0] decimal;
+
+BSD bs(.hex(total), .total(decimal));
 
 sevensegment ss(.datain(dataModule), .grounds(grounds), .display(display), .clk(clk));
 
@@ -28,22 +33,24 @@ always @(posedge clk)
 			2'b00:
 				begin
 					if(pbs == 2'b01)
-						begin
-							state <= 2'b01;
+						begin							
 							if(ds > 4'b0000 & ds <  4'b1011)
 								begin
 									total <= total - ds;
+									// dataModule <= {playerNum[3:0],decimal[11:0]};
 									dataModule <= {playerNum[3:0],total[11:0]};
 									if(playerNum == 4'b0001)
 										playerNum <= 4'b0010;
 									else
 										playerNum <= 4'b0001;
 									// total ve player ı yazdır
+									state <= 2'b01;
 								end
 							else
 								begin
 									minus <= 12'b000001100101;
 									dataModule <= {playerNum[3:0],minus[11:0]};
+									state <= 2'b01;
 								end
 						end
 					else if(pbs == 2'b10)
@@ -58,11 +65,15 @@ always @(posedge clk)
 				end
 			2'b01:
 				begin
-					state <= 2'b00;
+					if(pbs == 2'b00) 		// pushbutton released
+						state <= 2'b00;		// go back first state
+					else
+						state <= 2'b01;		// stay here
 				end
 			2'b10:
 				begin
-					state <= 2'b00;		
+					if(pbs == 2'b00)		// pb released
+						state <= 2'b00;		// else not mandatory
 				end
 			default: state <= state;
 		endcase
@@ -133,3 +144,44 @@ module sevensegment(datain, grounds, display, clk);
 	end
 endmodule
 
+module BSD(
+	input [11:0] hex,
+	output reg [11:0] total
+	);
+
+
+	reg [3:0] Hundreds;
+	reg [3:0] Tens;
+	reg [3:0] Ones;
+
+	integer i;
+
+	always @(hex)
+	begin
+		// set 100's, 10's and 1's to 0
+		Hundreds = 4'd0;
+		Tens = 4'd0;
+		Ones = 4'd0;
+
+		for(i=7; i>=0; i=i-1)
+		begin
+			// add 3 to columns >= 5
+			if(Hundreds >= 5)
+				Hundreds = Hundreds + 3;
+			if(Tens >= 5)
+				Tens = Tens + 3;
+			if(Ones >= 5)
+				Ones = Ones + 3;
+
+			// shift left one
+			Hundreds = Hundreds << 3;
+			Hundreds[0] = Tens[3];
+			Tens = Tens << 1;
+			Tens[0] = Ones[3];
+			Ones = Ones << 1;
+			Ones[0] = hex[i];
+
+			total = {Hundreds[3:0],Tens[3:0], Ones[3:0]};
+		end
+	end
+endmodule
