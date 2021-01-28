bdd1d158b070a4f98c1874f673fa5dfb3dfd1d64
Ömer Çakı <omerfarukcaki@gmail.com>
2021-01-28 21:03:27 +0300

    verilog assembly

--- Project2/program.asm
+++ Project2/program.asm
@@ -5,6 +5,7 @@
             ldi 3 2305
             ldi 7 450
 dsp         call bcd
+            ldi 6 0
 loop        ld 4 3
             ldi 5 1
             and 4 4 5
@@ -16,10 +17,15 @@ loop        ld 4 3
             ldi 5 15
             sub 5 5 4
             jz edt
-            ldi 5 0
-            add 5 5 4
-            jz loop
-            mov 6 4
+            push 0
+            push 4
+            ldi 0 10
+            mov 4 6
+            call mult2
+            mov 6 0
+            pop 4
+            pop 0
+            add 6 6 4
             st 1 6
             jmp loop
 edt         add 0 0 6
@@ -36,6 +42,18 @@ mul1        add 5 5 1
             dec 4
             jz multend
             jmp mul1
+mult2       push 1
+            jmp multi2
+multend2    pop 1
+            ret
+multi2      mov 4 6
+            mov 1 0
+            ldi 5 0
+mul2        add 5 5 1
+            mov 0 5
+            dec 4
+            jz multend2
+            jmp mul2
 bcd         push 0
             push 1
             push 2
