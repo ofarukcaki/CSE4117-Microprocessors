a9c3380df94418b51dab5a6d917d54962c374523
Ömer Çakı <omerfarukcaki@gmail.com>
2021-01-28 22:47:45 +0300

    Bird

---
+++ Project2/logism_final.asm
@@ -0,0 +1,142 @@
+.code
+            ldi 7 450
+            ldi 0 0
+loop        ldi 1 3327
+            ld 1 1
+            ldi 2 1
+            and 2 2 1
+            jz nxx
+            ldi 2 3326
+            ld 2 2
+            add 0 0 2
+            call bcd
+            jmp loop
+nxx         ldi 1 3839
+            ld 1 1
+            ldi 2 1
+            and 2 2 1
+            jz loop
+            ldi 2 3838
+            ld 2 2
+            push 0
+            mov 0 2
+            mov 6 2
+            call mult2
+            mov 2 0
+            pop 0
+            add 0 0 2
+            call bcd
+            jmp loop
+mult2       push 1
+            jmp multi2
+multend2    pop 1
+            ret
+multi2      mov 4 6
+            mov 1 0
+            ldi 5 0
+mul2        add 5 5 1
+            mov 0 5
+            dec 4
+            jz multend2
+            jmp mul2
+bcd         push 0
+            push 1
+            push 2
+            push 3
+            push 4
+            push 5
+            push 6
+            ldi 1 0
+            mov 3 0
+            ldi 2 8
+start       ldi 6 15 
+            and 6 1 6
+            ldi 5 0
+            sub 5 6 5
+            jz skip3
+            ldi 5 1
+            sub 5 6 5
+            jz skip3
+            ldi 5 2
+            sub 5 6 5
+            jz skip3
+            ldi 5 3
+            sub 5 6 5
+            jz skip3
+            ldi 5 4
+            sub 5 6 5
+            jz skip3
+            ldi 5 3
+            add 1 1 5
+skip3       ldi 6 240
+            and 6 1 6
+            ldi 5 0
+            sub 5 6 5
+            jz skip32
+            ldi 5 16
+            sub 5 6 5
+            jz skip32
+            ldi 5 32
+            sub 5 6 5
+            jz skip32
+            ldi 5 48
+            sub 5 6 5
+            jz skip32
+            ldi 5 64
+            sub 5 6 5
+            jz skip32
+            ldi 5 48
+            add 1 1 5
+skip32      ldi 6 3840
+            and 6 1 6
+            ldi 5 0
+            sub 5 6 5
+            jz skip33
+            ldi 5 256
+            sub 5 6 5
+            jz skip33
+            ldi 5 512
+            sub 5 6 5
+            jz skip33
+            ldi 5 768
+            sub 5 6 5
+            jz skip33
+            ldi 5 1024
+            sub 5 6 5
+            jz skip33
+            ldi 5 768
+            add 1 1 5
+skip33      add 1 1 1
+            mov 5 2
+            dec 5
+            jz handlelast
+            ldi 4 1
+ll          add 4 4 4
+            dec 5
+            jz exitll
+            jmp ll
+exitll      and 4 3 4
+            jz iszero
+            ldi 5 1
+            add 1 1 5
+iszero      dec 2
+            jz endbcd
+            jmp start
+endbcd      mov 0 1
+            pop 6
+            pop 5
+            pop 4
+            pop 3
+            pop 2
+            pop 1
+            ldi 2 3549
+            st 2 0
+            pop 0
+            ret
+uzay        jmp uzay
+handlelast  ldi 5 1
+            and 5 3 5
+            jz endbcd
+            ldi 5 1
+            add 1 1 5
+            jmp endbcd
